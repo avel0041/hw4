ca41a6ed9841b55f071de93a82b53d4be2303356
avel0041 <vel-ar@mail.ru>
2020-06-04 15:08:16 +0300

    First

---
+++ formatter_ex_lib/_build2/CMakeFiles/formatter_ex.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/art/avel0041/workspace/projects/homework03/formatter_ex_lib/formatter_ex.cpp" "/home/art/avel0041/workspace/projects/homework03/formatter_ex_lib/_build2/CMakeFiles/formatter_ex.dir/formatter_ex.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../../formatter_lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/art/avel0041/workspace/projects/homework03/formatter_ex_lib/_build2/sub/CMakeFiles/formatter.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
